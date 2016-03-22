007d22ef87a93829d2272c8ac9b067ecca3d7c12
Samuel Cooledge <scooledge@gmail.com>
2016-03-21 19:13:04 -0700

    Added files via upload

--- orgMap2.js
+++ orgMap2.js
@@ -1,39 +1,25 @@
 
 function newFunction() {    /* Map Object */
-      var mapObj = new GMaps({
-        el: '#map',
-  lat: 47.622868,
-  lng: -122.336553,
-  click: function(e) {
-    alert('You clicked on the map');
-  },
-  zoom_changed: function(e) {
-    alert('You zoomed the map');
-  }
+
+       
+var mapObj = new GMaps({
+  el: '#map',
+  lat: 48.857,
+  lng: 2.295,
+});
   
-});
-var m = mapObj.addMarker({
-  lat: 47.622868,
-  lng: -122.336553,
-  title: 'Amazon',
-  infoWindow: {
-    content: '<h4>Amazon Headquarters</h4><div>440 Terry Ave N, Seattle, WA 98109</div><div>Seattle, Wa</div>',
-    maxWidth: 100
-  }
-});
-mapObj.addMarker({
-  lat: 48.8553701,
-  lng: 2.2944813,
-  mouseover: function(e) {
-    alert('Triggered');
+
+
+GMaps.geocode({
+  
+  address: document.getElementById("address").value,
+  callback: function(results, status) {
+    if (status == 'OK') {
+      latlng = results[0].geometry.location;
+      mapObj.setCenter(latlng.lat(), latlng.lng());
+    } else if (status == 'ZERO_RESULTS') {
+      alert('Sorry, no results found');
+    }
   }
 });
 };
-$(document).ready(function(){
-    $("button").click(function(){
-        $("#div1").fadeTo("slow", 0.15);
-        $("#div2").fadeTo("slow", 0.4);
-        $("#div3").fadeTo("slow", 0.7);
-    });
-});
-
